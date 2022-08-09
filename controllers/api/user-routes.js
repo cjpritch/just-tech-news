6588aafe3e2515e917a745895e9a9f42aeb8591f
Carly Pritchard <cpritch616@gmail.com>
2022-08-09 16:34:16 -0500

    create edit, update, delete routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -128,7 +130,7 @@ router.put('/:id', (req, res) => {
   });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
       where: {
         id: req.params.id
